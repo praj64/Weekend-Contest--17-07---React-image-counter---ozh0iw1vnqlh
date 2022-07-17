0c8557b272524d94a752325c980d5405e3d7ad13
Prajakta Tawde <98815167+praj64@users.noreply.github.com>
2022-07-17 12:43:37 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,8 @@ const App = () => {
   return (
     <div id="main">
       <img src={star}  height="300px" width="300px" />
+      <img src={star}  height="302px" width="302px" />
+      <img src ={star} height="304px" width="304px" />
     </div>
   )
 }
